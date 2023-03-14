cd160f28c63e6e5d99370fa4212c141422666da2
Nouheila <76542107+nounou-nyoki@users.noreply.github.com>
2023-03-14 16:38:45 +0100

    initial commit

--- src/Controller/NonoController.php
+++ src/Controller/NonoController.php
@@ -3,17 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class NonoController extends AbstractController
 {
     #[Route('/', name: 'app_nono')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
         return new Response(<<<E0F
             <html>
                 <body>
+                    $greet
                     <img src="/images/under-construction.gif" />
                 </body>
             </html>
